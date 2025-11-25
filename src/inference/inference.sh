21b89f15b801e95f6c3ef360c410f47254e95363
Blood-Soul <baijun139@gmail.com>
2025-11-25 11:16:51 +0800

    fix: fix some bug during preparing data

--- src/inference/inference.sh
+++ src/inference/inference.sh
@@ -1,4 +1,4 @@
-python inference_qa.py \
+python src/inference/inference_qa.py \
     --input_data data/original_data/jec/prepared_parquet/JEC_1_multi_choice_test_prepared.parquet \
     --output_dir data/infer_answer \
     --output_name JEC_1_multi_choice_test_inferenced \
